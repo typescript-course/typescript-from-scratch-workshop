cf506f039fa620243fd159f226bb1460dfa62306
Joe Previte <jjprevite@gmail.com>
2022-05-19 22:46:31 +0000

    fixup: use correct wordWrap test

--- lesson-06-intro-tdd/__tests__/index.test.ts
+++ lesson-06-intro-tdd/__tests__/index.test.ts
@@ -3,10 +3,10 @@ import { wordWrap } from "../src";
 // If the row-length is 60, and the input string is 30, the result is just the input string
 describe("wordWrap", () => {
   it("should return the input string when row-length > input string", () => {
-    const rowLength = 60
-    const inputStr = "hello"
-    const actual = wordWrap(rowLength, inputStr)
-    const expected = inputStr
-    expect(actual).toBe(expected);
+    const rowLength = 60;
+    const inputStr = "hello";
+    const actual = wordWrap(rowLength, inputStr);
+    const expected = [inputStr];
+    expect(actual).toStrictEqual(expected);
   });
 });
